7aa44a956f72e087981ac0d27e8cd9834d9192df
No1Cheats <jan.imhof@gmail.com>
2020-09-05 10:37:29 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,54 @@
+# A2B Dispatch v1
+# Made by Jan Imhof
+
+import json
+import discord
+import geopy.distance
+import requests
+from discord.ext import commands
+
+# Opening the config.json file that stores the Discord Token and avwx Auth code
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+TOKEN = config['token']  # Psst keep secret
+client = commands.Bot(command_prefix='!')  # Meaning that all commands must start with a ! so for instance !metar
+client.remove_command('help')  # removes the custom help command
+
+
+def get_station(loc):
+    url = 'https://avwx.rest/api/station/' + loc + '?format=json'
+    headers = {'Authorization': config['authHeader']}
+    r = requests.request("GET", url, headers=headers)
+    data = r.json()
+    return data
+
+
+def get_distance(loc1, loc2):
+    station1 = get_station(loc1)
+    station2 = get_station(loc2)
+    coords_1 = (station1['latitude'], station1['longitude'])
+    coords_2 = (station2['latitude'], station2['longitude'])
+    return geopy.distance.geodesic(coords_1, coords_2).nm
+
+
+@client.command()
+async def help(ctx):
+    help_embed = discord.Embed(
+        title="A2B Bot",
+        description='See the commands list below',
+        color=discord.Colour.orange()
+    )
+    # Setting all the Values
+    help_embed.add_field(name='Request a quote using: \n!quote registration origin destination ',
+                         value='!quote N828SY KJFK KBOS', inline=False)
+    await ctx.send(embed=help_embed)
+
+
+@client.command()
+async def quote(ctx, registration, origin, destination):
+    distance = get_distance(origin, destination)
+
+
+
+client.run(TOKEN)
