99bc4f8097a3a1a1643e4d79202b44e5d4b7ab03
No1Cheats <jan.imhof@gmail.com>
2020-09-05 10:54:45 +0200

    added most basic functions

--- bot.py
+++ bot.py
@@ -6,13 +6,14 @@ import discord
 import geopy.distance
 import requests
 from discord.ext import commands
+import xml.etree.ElementTree as ET
 
 # Opening the config.json file that stores the Discord Token and avwx Auth code
 with open('config.json') as config_file:
     config = json.load(config_file)
 
 TOKEN = config['token']  # Psst keep secret
-client = commands.Bot(command_prefix='!')  # Meaning that all commands must start with a ! so for instance !metar
+client = commands.Bot(command_prefix='!')  # Meaning that all commands must start with a ! so for instance !quote
 client.remove_command('help')  # removes the custom help command
 
 
@@ -29,7 +30,15 @@ def get_distance(loc1, loc2):
     station2 = get_station(loc2)
     coords_1 = (station1['latitude'], station1['longitude'])
     coords_2 = (station2['latitude'], station2['longitude'])
-    return geopy.distance.geodesic(coords_1, coords_2).nm
+    return round(geopy.distance.geodesic(coords_1, coords_2).nm)
+
+
+def get_plane(registration):
+    url = 'https://server.fseconomy.net/data?userkey=' + config[
+        'datafeed'] + '&format=xml&query=aircraft&search=registration&aircraftreg=' + registration
+    response = requests.request("GET", url)
+    root = ET.fromstring(response.content)
+    print(root[0][9].text)  # [0][9] is the Equipment tag
 
 
 @client.command()
@@ -37,7 +46,7 @@ async def help(ctx):
     help_embed = discord.Embed(
         title="A2B Bot",
         description='See the commands list below',
-        color=discord.Colour.orange()
+        color=discord.Colour.red()
     )
     # Setting all the Values
     help_embed.add_field(name='Request a quote using: \n!quote registration origin destination ',
@@ -47,8 +56,15 @@ async def help(ctx):
 
 @client.command()
 async def quote(ctx, registration, origin, destination):
+    get_plane(registration)
     distance = get_distance(origin, destination)
-
+    quote_embed = discord.Embed(
+        title="A2B Bot",
+        description='See your quote below',
+        color=discord.Colour.red()
+    )
+    quote_embed.add_field(name="Distance: ", value=distance, inline=False)
+    await ctx.send(embed=quote_embed)
 
 
 client.run(TOKEN)
